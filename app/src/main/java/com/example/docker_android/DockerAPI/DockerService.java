49e2776f54f36dfc3dee39166a8234a73e533562
xy815661276 <815661276@qq.com>
2020-02-10 19:41:52 +0800

    更换原来的docker-client,改用api接口来编写app。

---
+++ app/src/main/java/com/example/docker_android/DockerAPI/DockerService.java
@@ -0,0 +1,55 @@
+package com.example.docker_android.DockerAPI;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * 容器服务类
+ */
+public class DockerService {
+    private static String address = "http://121.36.19.122:2375";
+    /**
+     * 获取容器列表，GET方法
+     * @param callback 回调方法
+     */
+    public static void getContainers(okhttp3.Callback callback) {
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder()
+                .url(address+"/containers/json")
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+
+    /**
+     * 获取一个容器的信息，GET方法
+     * @param id 容器id
+     * @param callback 回调方法
+     */
+    public static void getContainer(String id,okhttp3.Callback callback) {
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder()
+                .url(address+"/containers/"+id+"/json")
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+    /**
+     * /containers/create
+     */
+    public static void createContainer(String id,okhttp3.Callback callback) {
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder()
+                .url(address+"/containers/"+id+"/json")
+
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+}
--- app/src/main/java/com/example/docker_android/MainActivity.java
+++ app/src/main/java/com/example/docker_android/MainActivity.java
@@ -3,30 +3,43 @@ package com.example.docker_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.example.docker_android.DockerUtils.DockerClientService;
-import com.spotify.docker.client.DefaultDockerClient;
-import com.spotify.docker.client.DockerClient;
-import com.spotify.docker.client.exceptions.DockerCertificateException;
-import com.spotify.docker.client.exceptions.DockerException;
-import com.spotify.docker.client.messages.Container;
+import com.example.docker_android.DockerAPI.DockerService;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import okhttp3.Call;
+import okhttp3.Response;
 
+public class MainActivity extends AppCompatActivity {
+    private TextView text;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.test);
+        button = findViewById(R.id.test_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DockerService.getContainers(new okhttp3.Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
 
-        try {
-            DockerClientService dockerClientService = new DockerClientService();
-            dockerClientService.getDockerInfo();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+                    }
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        System.out.println(response.body().string());
+                    }
+                });
+            }
+        });
     }
-
-
 }
